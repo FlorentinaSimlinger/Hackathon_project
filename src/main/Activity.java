537360cc92f501048513e9191287daa1f600df78
FlorentinaSimlinger <60526092+FlorentinaSimlinger@users.noreply.github.com>
2020-02-01 14:47:33 -0800

    basic structure setup

---
+++ src/main/Activity.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Activity {
+    private String location;
+    private String activity;
+
+    public Activity(String activity, String location) {
+
+    }
+}
---
+++ src/test/ActivitiesNavigatorTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import main.Activity;
+import org.junit.jupiter.api.BeforeEach;
+
+class ActivitiesNavigatorTest {
+    Activity study;
+    Activity eat;
+
+    @BeforeEach
+    void runBefore() {
+        
+    }
+
+}
+
+
